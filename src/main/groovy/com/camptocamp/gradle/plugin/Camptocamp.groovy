490bbf6affa0efcc7ea5aec1d2c6cdda25d3ab5d
Jesse Eichar <jeichar@ktunaxa.local>
2010-03-25 22:02:25 +0100

    split filtering into two tasks and tried to add out put to war

--- src/main/groovy/com/camptocamp/gradle/plugin/Camptocamp.groovy
+++ src/main/groovy/com/camptocamp/gradle/plugin/Camptocamp.groovy
@@ -5,14 +5,32 @@ import org.gradle.api.plugins.*;
 import com.camptocamp.gradle.task.*
 
 class Camptocamp implements Plugin<Project> {  
-  
+    
+    final String FILTER_RESOURCES_NAME = "filterResources"
+    final String FILTER_RESOURCES_IN = "src/main/filtered-resources"
+    final String FILTER_RESOURCES_OUT = "classes/main/"
+
+    final String FILTER_WEBAPP_NAME = "filterWebapp"
+    final String FILTER_WEBAPP_IN = "src/main/filtered-webapp"
+    final String FILTER_WEBAPP_OUT = "filtered/webapp"
+    
     def void use(Project project) {
         project.convention.plugins.camptocamp = new CamptocampConvention()
-        
+
         configureProjectLayout(project)
-        configureFiltering(project)
+
+        configureFiltering(project, FILTER_RESOURCES_NAME, FILTER_RESOURCES_IN, FILTER_RESOURCES_OUT)
+        configureFiltering(project, FILTER_WEBAPP_NAME, FILTER_WEBAPP_IN, FILTER_WEBAPP_OUT)
+
         configureWarProjectLayout(project)
         configureAddSecurityProxy(project)
+        configureWarPlugins(project)
+    }
+
+    def configureWarPlugins(Project project) {
+         project.tasks.withType(org.gradle.api.tasks.bundling.War.class).allTasks {
+             task.from pluginConvention.getWebAppDir();
+         }
     }
     
     def configureProjectLayout(Project project) {
@@ -20,17 +38,19 @@ class Camptocamp implements Plugin<Project> {
         layout.description = "Adds files for a default Camptocamp application configuration"
     }
     
-    def configureFiltering(Project project) {
-        Filtering filtering = project.tasks.add("filtering", Filtering.class)
+    def configureFiltering(Project project, String name, String input, String output) {
+        Filtering filtering = project.tasks.add("$project.buildDir/$name", Filtering.class)
         filtering.description = "copies all the files that need to have strings updated from the "+
                                 "filter files to the build dir for inclusion into the webapp"
         
-        filtering.from "src/main/filtered-resources"
-        filtering.from "src/main/filtered-webapp"
-        filtering.into "$project.buildDir/filtered"
+        filtering.from input
+        filtering.into output
         
         project.ant.delete(dir: "$project.buildDir/filtered")
         
+        if(System.getProperty("server") == null) {
+            project.logger.info("system property 'server' is not defined so defaulting to 'local'")
+        }
         def server = System.getProperty("server") ?: "local"
         
         project.fileTree {
@@ -44,7 +64,7 @@ class Camptocamp implements Plugin<Project> {
         if (global.exists()) {
             filtering.filterFile(global)
         } else {
-            project.logger.warn("$global does not exist. Verify this is not an error")
+            project.logger.info("$global does not exist. Verify this is not an error")
         }
         
     }
